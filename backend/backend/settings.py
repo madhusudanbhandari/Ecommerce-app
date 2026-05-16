b65b78944a30acb293de28f3dd43f2bf757e2fb2
Madhusudan Bhandari <madhusudanb636@gmail.com>
2026-05-16 10:15:52 +0545

    fix

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from pathlib import Path
 import os
 from datetime import timedelta
+import dj_database_url
 from dotenv import load_dotenv
 load_dotenv()
 
